4031bb737ac6d147c0a9284fcd6bc152c22368d7
Allister Rooke <allisterg@gmail.com>
2018-11-26 11:26:14 -0500

    week checkboxes connected

--- src/components/week-habits.js
+++ src/components/week-habits.js
@@ -9,18 +9,23 @@ export class weekHabits extends React.Component {
         this.props.dispatch(fetchHabits())
     }
 
+    checkHabit(day, id) {
+        console.log(id);
+        this.props.dispatch(checkHabitAction(day, id))
+    }
+    
     render() {
         let days = Object.keys(this.props.days)
         console.log(days)
         let dayList = days.map(day => <span>{day[0]}</span>)
         let habitsList = this.props.days[this.props.day].habits.map(habit => <p>{habit.habit}
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
+        <input onChange={() => this.checkHabit("Monday", habit.id)} type="checkbox" checked={this.props.days.Monday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Tuesday", habit.id)} type="checkbox" checked={this.props.days.Tuesday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Wednesday", habit.id)} type="checkbox" checked={this.props.days.Wednesday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Thursday", habit.id)} type="checkbox" checked={this.props.days.Thursday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Friday", habit.id)} type="checkbox" checked={this.props.days.Friday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Saturday", habit.id)} type="checkbox" checked={this.props.days.Saturday.habits[habit.id].checked} />
+        <input onChange={() => this.checkHabit("Sunday", habit.id)} type="checkbox" checked={this.props.days.Sunday.habits[habit.id].checked} />
         </p>)
         return (
             <div>
